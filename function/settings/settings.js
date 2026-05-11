cdff5064a26a297632a34672312453d6f5199265
alilalal686-eng <alilalal686@gmail.com>
2026-05-11 22:47:06 +0100

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,7 +14,7 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
+ nomerbot : '+212601025572',
  pairingNumber : '212605784394',
  figlet: 'silanalite', // create a start console display
  nomorwa : '212605784394',
